c12eb43cb43a51fd0616e1ce843c8706481d25ac
andrewmenich <andrewmenich@gmail.com>
2018-01-16 13:07:21 -0500

    full first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp'),
+browserSync = require('browser-sync').create(),
+sass = require('gulp-sass'),
+ autoprefixer = require('gulp-autoprefixer');
+
+// Static server & watch scss + html files
+gulp.task('watch', ['sass'], function() {
+
+browserSync.init({
+server: '.'
+});
+
+gulp.watch('./scss/**/*.scss', ['sass'], browserSync.reload);
+gulp.watch('./*.html').on('change', browserSync.reload);
+gulp.watch('./js/**/*.js', browserSync.reload);
+
+});
+
+// Compile Sass into CSS & inject into browsers
+gulp.task('sass', function() {
+return gulp.src('./scss/**/*.scss')
+.pipe(sass().on('error', sass.logError))
+.pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+}))
+.pipe(gulp.dest('./css'))
+.pipe(browserSync.stream());
+});
+
+
+// default will also watch
+gulp.task('default', ['watch']);
+
